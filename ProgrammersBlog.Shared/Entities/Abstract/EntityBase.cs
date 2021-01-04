182f233acc0059a77144e6c7c8f9bd8d4e4126de
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-01-04 09:06:33 +0300

    EntityBase abstract sınıfı ve IEntity arayüzü eklendi ve tasarlandı

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
+    }
+}
