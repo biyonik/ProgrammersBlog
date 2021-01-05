825f962b33bc450eca767d7ed8f85dc92f6ec6e2
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-01-05 21:19:04 +0300

    Context eklendi, Repository katmanı düzenlendi, Mapping işlemleri başlatıldı

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
+{
+    public class ProgrammersBlogContext: DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-NOOAEV8\SQLEXPRESS;Database=ProgrammersBlogDB;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class ArticleMap: IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.ToTable("Articles");
+            builder.HasKey(a => a.Id);
+            
+            builder.Property(a => a.Id)
+                .ValueGeneratedOnAdd();
+            
+            builder.Property(a => a.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+            
+            builder.Property(a => a.Content)
+                .IsRequired()
+                .HasColumnName("NVARCHAR(MAX)");
+            
+            builder.Property(a => a.Date)
+                .IsRequired();
+            
+            builder.Property(a => a.SeoAuthor)
+                .IsRequired()
+                .HasMaxLength(50);
+            
+            builder.Property(a => a.SeoDescription)
+                .HasMaxLength(150)
+                .IsRequired();
+            
+            builder.Property(a => a.SeoTags)
+                .IsRequired()
+                .HasMaxLength(70);
+            
+            builder.Property(a => a.ViewsCount)
+                .IsRequired();
+            
+            builder.Property(a => a.CommentCount)
+                .IsRequired();
+
+            builder.Property(a => a.Thumbnail)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.Property(a => a.CreatedByName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(a => a.ModifiedByName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(a => a.CreatedDate)
+                .IsRequired();
+            
+            builder.Property(a => a.ModifiedDate)
+                .IsRequired();
+
+            builder.Property(a => a.IsActive)
+                .IsRequired();
+
+            builder.Property(a => a.IsDeleted)
+                .IsRequired();
+
+            builder.Property(a => a.Note)
+                .HasMaxLength(500);
+
+            builder.HasOne<Category>(a => a.Category)
+                .WithMany(c => c.Articles)
+                .HasForeignKey(a => a.CategoryId);
+
+            builder.HasOne<User>(a => a.User)
+                .WithMany(u => u.Articles)
+                .HasForeignKey(a => a.UserId);
+        }
+    }
+}
